62632fdeed92ce46d2b5b31afd1673e01f3b8c79
Sojung Lee <sojung.lee2@georgebrown.ca>
2021-11-18 23:23:53 -0500

    updated gdd and button behavior

--- GAME2014_A2/Assets/Scripts/ButtonBehavior.cs
+++ GAME2014_A2/Assets/Scripts/ButtonBehavior.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/*Source File Name: ButtonBehavior
+ * Author's Name: Sojung (Serena) Lee
+ * Student #: 101245044
+ * Date Last Modified: November 18, 2021
+ * Program Description: Sets up the button behaviors to change the current scene
+ * Revision History: (November 18) Added different button functions for main menu and game over
+ */
+
 public class ButtonBehavior : MonoBehaviour
 {
     public void StartGame()
