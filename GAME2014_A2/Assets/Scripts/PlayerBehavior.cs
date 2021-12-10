8b5c17628c39803475e993b331c93e64be9212c1
Sojung Lee <sojung.lee2@georgebrown.ca>
2021-12-10 13:26:21 -0500

    Updated player, reformatted instructions scene, and added virtual joysticks

---
+++ GAME2014_A2/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,134 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerAnimationState
+{
+    IDLE,
+    WALK,
+    JUMP,
+    NUM_OF_ANIMS
+}
+
+public class PlayerBehavior : MonoBehaviour
+{
+    [Header("Touch Input")]
+    public Joystick joystick;
+    [Range(0.01f, 1.0f)]
+    public float sensitivity;
+
+    [Header("Movement")]
+    public float horizontalForce;
+    public float verticalForce;
+    public bool isGrounded;
+    public Transform groundOrigin;
+    public float groundRadius;
+    public LayerMask groundLayerMask;
+    [Range(0.1f, 0.9f)]
+    public float airControlFactor;
+
+    [Header("Animation")]
+    public PlayerAnimationState state;
+
+    private Rigidbody2D rb;
+    private Animator animatorController;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animatorController = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        CheckIfGrounded();
+    }
+
+    private void Move()
+    {
+        float x = (Input.GetAxisRaw("Horizontal") + joystick.Horizontal) * sensitivity;
+
+        if (isGrounded)
+        {
+            float y = (Input.GetAxisRaw("Vertical") + joystick.Vertical) * sensitivity;
+            float jump = Input.GetAxisRaw("Jump") + ((UIController.jumpButtonDown) ? 1.0f : 0.0f);
+
+            if (x != 0)
+            {
+                x = FlipAnimation(x);
+                animatorController.SetInteger("AnimationState", (int)PlayerAnimationState.WALK); // WALK State
+                state = PlayerAnimationState.WALK;
+            }
+            else
+            {
+                animatorController.SetInteger("AnimationState", (int)PlayerAnimationState.IDLE); // IDLE State
+                state = PlayerAnimationState.IDLE;
+            }
+
+            float horizontalMoveForce = x * horizontalForce;
+            float jumpMoveForce = jump * verticalForce;
+
+            float mass = rb.mass * rb.gravityScale;
+
+
+            rb.AddForce(new Vector2(horizontalMoveForce, jumpMoveForce) * mass);
+            rb.velocity *= 0.99f; // scaling / stopping hack
+        }
+        else // Air Control
+        {
+            animatorController.SetInteger("AnimationState", (int)PlayerAnimationState.JUMP); // JUMP State
+            state = PlayerAnimationState.JUMP;
+
+            if (x != 0)
+            {
+                x = FlipAnimation(x);
+
+                float horizontalMoveForce = x * horizontalForce * airControlFactor;
+                float mass = rb.mass * rb.gravityScale;
+
+                rb.AddForce(new Vector2(horizontalMoveForce, 0.0f) * mass);
+            }
+        }
+
+    }
+
+    private void CheckIfGrounded()
+    {
+        RaycastHit2D hit = Physics2D.CircleCast(groundOrigin.position, groundRadius, Vector2.down, groundRadius, groundLayerMask);
+
+        isGrounded = (hit) ? true : false;
+    }
+
+    private float FlipAnimation(float x)
+    {
+        // depending on direction scale across the x-axis either 1 or -1
+        x = (x > 0) ? 1 : -1;
+
+        transform.localScale = new Vector3(x, 1.0f);
+        return x;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            transform.SetParent(other.transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            transform.SetParent(null);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(groundOrigin.position, groundRadius);
+    }
+
+}
